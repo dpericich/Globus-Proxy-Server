d222209b2d25cdd170b354962644a97c69a73263
Scott L <ehee27@gmail.com>
2024-08-11 20:00:59 -0500

    tours folder with All Tours, or individual (GLobus, Avalon, Cosmos), TourCard and TourMedia Details Components, and client connect Modal

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ const SearchBar = ({ search, setSearch }) => {
     <div className="bg-sky-800 flex justify-start p-3">
       <form
         onSubmit={e => e.preventDefault()}
-        className="flex justify-start w-[50%]"
+        className="flex justify-start w-[100%]"
       >
         <input
           id="search"
@@ -12,7 +12,7 @@ const SearchBar = ({ search, setSearch }) => {
           placeholder="search tours"
           value={search}
           onChange={e => setSearch(e.target.value)}
-          className="rounded-md py-1 px-3 w-[80%]"
+          className="rounded-md py-1 px-3 w-[100%]"
         />
       </form>
     </div>
--- client/src/components/TourCard.jsx
+++
@@ -1,9 +0,0 @@
-const TourCard = ({ tour }) => {
-  return (
-    <div className="flex justify-center items-center shadow-md shadow-zinc-300 rounded-xl p-3 mb-2 hover:cursor-pointer hover:bg-zinc-100 transition-all">
-      {tour.Name}
-    </div>
-  )
-}
-
-export default TourCard
