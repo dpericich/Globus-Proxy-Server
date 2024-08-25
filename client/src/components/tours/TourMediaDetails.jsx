841defcecaf005e900971972daf349d173a3fe8f
Daniel Pericich <dpericich@gmail.com>
2024-08-25 16:41:57 -0500

    Implemented conditional rendering based on API call for Tour Media Details component

--- client/src/components/tours/TourMediaDetails.jsx
+++ client/src/components/tours/TourMediaDetails.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import ErrorBoundryTours from './ErrorBoundryTours'
 import Loading from '../Loading'
 import Modal from './Modal'
 import Success from './Success'
@@ -9,43 +10,44 @@ const TourMediaDetails = () => {
   const { id } = useParams()
   const [selectedTour, setSelectedTour] = useState(null)
   const [open, setOpen] = useState(false)
-  const [noData, setNoData] = useState(true)
-  const [failedState, setFailedState] = useState(false)
+  const [isLoading, setIsLoading] = useState(true);
+  const [isFailed, setIsFailed] = useState(false)
 
   useEffect(() => {
-    fetch('http://localhost:8000/api/v1/globus/get-tour-media', {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      body: JSON.stringify({ tourNumber: id }),
-    })
-      .then(res => res.json())
-      .then(res => {
-        setSelectedTour(res), setNoData(false)
+    try {
+      fetch('http://localhost:8000/api/v1/globus/get-tour-media', {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({ tourNumber: id }),
       })
-      .catch(e => {
-        console.log(e)
-        setFailedState(true)
-      })
+        .then(res => res.json())
+        .then(res => {
+          setSelectedTour(res);
+          setIsLoading(false);
+        })
+    } catch (e) {
+      setIsLoading(false);
+      setIsFailed(true);
+    }
   }, [])
 
   return (
-    <div className="py-3">
-      <div>
-        {noData === true && failedState === false && <Loading />}
-        {noData === false && failedState === false && (
-          <Success selectedTour={selectedTour} setOpen={setOpen} id={id} />
-        )}
-        {noData === true && failedState === true && <Failed />}
-      </div>
+    <ErrorBoundryTours fallback={isLoading === false && <Failed />}>
+      <div className="py-3">
+        <div>
+          {isLoading === true ? < Loading /> : null}
+          {isLoading === false && <Success selectedTour={selectedTour} setOpen={setOpen} open={open} id={id} />}
+        </div>
 
-      <Modal
-        open={open}
-        onClose={() => setOpen(!open)}
-        selectedTour={selectedTour}
-      />
-    </div>
+        <Modal
+          open={open}
+          onClose={() => setOpen(!open)}
+          selectedTour={selectedTour}
+        />
+      </div>
+    </ErrorBoundryTours>
   )
 }
 export default TourMediaDetails
